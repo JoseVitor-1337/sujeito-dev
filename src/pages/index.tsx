d5dcd727fa026a1f36b560735b75652095f2b660
JoseVitor-1337 <josevitordemelo@yahoo.com>
2021-07-20 14:24:39 -0300

    Feature: instalando SASS.

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Sujeito Dev - Home</title>
+      </Head>
+      <div>
+        <h1>teste</h1>
+      </div>
+    </>
+  );
+}
